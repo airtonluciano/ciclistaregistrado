6ee4e78ba5c82947abc19172cd9d9514611db5e0
airtonluciano <airtonluciano@gmail.com>
2021-04-12 15:15:46 -0300

    + uma home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Ciclista @Registrado</h1><p>Registre sua bike!</p>
+    return <div><h1>Ciclista @Registrado</h1><p>Registre sua bike!</p></div>
 }
 
 export default Home
